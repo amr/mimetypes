a39ac87b65fce73dbf69e4e52fa34a48aa2b9de4
Amr Mostafa <amr.mostafa@gmail.com>
2015-12-05 13:37:30 +0200

    Switch to 2 spaces code style

--- src/main/java/com/github/amr/mimetypes/MimeType.java
+++ src/main/java/com/github/amr/mimetypes/MimeType.java
@@ -3,56 +3,64 @@ package com.github.amr.mimetypes;
 import java.util.Arrays;
 
 public class MimeType {
-	public MimeType() {
-	}
+  public MimeType() {
+  }
 
-	public MimeType(String mimeType) {
-		this.mimeType = mimeType;
-		this.extensions = new String[0];
-	}
+  public MimeType(String mimeType) {
+    this.mimeType = mimeType;
+    this.extensions = new String[0];
+  }
 
-	public MimeType(String mimeType, String[] extensions) {
-		this.mimeType = mimeType;
-		this.extensions = extensions;
-	}
+  public MimeType(String mimeType, String[] extensions) {
+    this.mimeType = mimeType;
+    this.extensions = extensions;
+  }
 
-	private String mimeType;
+  private String mimeType;
 
-	private String[] extensions;
+  private String[] extensions;
 
-	public String getMimeType() {
-		return mimeType;
-	}
+  public String getMimeType() {
+    return mimeType;
+  }
 
-	public String[] getExtensions() {
-		return extensions;
-	}
+  public String[] getExtensions() {
+    return extensions;
+  }
 
-	public String getExtension() {
-		if (extensions != null && extensions.length > 0) {
-			return extensions[0];
-		}
+  public String getExtension() {
+    if (extensions != null && extensions.length > 0) {
+      return extensions[0];
+    }
 
-		return null;
-	}
+    return null;
+  }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) return true;
-		if (o == null || getClass() != o.getClass()) return false;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
 
-		MimeType mimeType1 = (MimeType) o;
+    MimeType mimeType1 = (MimeType) o;
 
-		if (!Arrays.equals(extensions, mimeType1.extensions)) return false;
-		if (!mimeType.equals(mimeType1.mimeType)) return false;
+    if (!Arrays.equals(extensions, mimeType1.extensions)) {
+      return false;
+    }
+    if (!mimeType.equals(mimeType1.mimeType)) {
+      return false;
+    }
 
-		return true;
-	}
+    return true;
+  }
 
-	@Override
-	public int hashCode() {
-		int result = mimeType.hashCode();
-		result = 47 * result + Arrays.hashCode(extensions);
-		return result;
-	}
+  @Override
+  public int hashCode() {
+    int result = mimeType.hashCode();
+    result = 47 * result + Arrays.hashCode(extensions);
+    return result;
+  }
 }
--- src/test/java/com/github/amr/mimetypes/MimeTypesTest.java
+++ src/test/java/com/github/amr/mimetypes/MimeTypesTest.java
@@ -9,109 +9,109 @@ import java.nio.file.Paths;
 import static org.junit.Assert.*;
 
 public class MimeTypesTest {
-	public static Path getResource(String path) {
-		try {
-			return Paths.get(MimeTypesTest.class.getResource(path).toURI());
-		} catch (URISyntaxException e) {
-			throw new IllegalArgumentException(e);
-		}
-	}
+  public static Path getResource(String path) {
+    try {
+      return Paths.get(MimeTypesTest.class.getResource(path).toURI());
+    } catch (URISyntaxException e) {
+      throw new IllegalArgumentException(e);
+    }
+  }
 
-	@Test
-	public void testByType() {
-		// JSON
-		MimeType type = MimeTypes.getInstance().getByType("application/json");
-		assertEquals(type.getMimeType(), "application/json");
-		assertArrayEquals(type.getExtensions(), new String[]{"json"});
-		assertEquals(type.getExtension(), "json");
-	}
+  @Test
+  public void testByType() {
+    // JSON
+    MimeType type = MimeTypes.getInstance().getByType("application/json");
+    assertEquals(type.getMimeType(), "application/json");
+    assertArrayEquals(type.getExtensions(), new String[]{"json"});
+    assertEquals(type.getExtension(), "json");
+  }
 
-	@Test
-	public void testByExt() {
-		MimeType type = MimeTypes.getInstance().getByExtension("html");
-		assertEquals(type.getMimeType(), "text/html");
-		assertArrayEquals(type.getExtensions(), new String[]{"html", "htm"});
-		assertEquals(type.getExtension(), "html");
-	}
+  @Test
+  public void testByExt() {
+    MimeType type = MimeTypes.getInstance().getByExtension("html");
+    assertEquals(type.getMimeType(), "text/html");
+    assertArrayEquals(type.getExtensions(), new String[]{"html", "htm"});
+    assertEquals(type.getExtension(), "html");
+  }
 
-	@Test
-	public void testCustomDefinitionsFile() {
-		MimeTypes mimeTypes = MimeTypes.getInstance();
-		mimeTypes.load(getResource("custom-mime-types"));
+  @Test
+  public void testCustomDefinitionsFile() {
+    MimeTypes mimeTypes = MimeTypes.getInstance();
+    mimeTypes.load(getResource("custom-mime-types"));
 
-		// By Type
-		MimeType type = mimeTypes.getByType("application/vnd.custom.foo");
-		assertEquals(type.getMimeType(), "application/vnd.custom.foo");
-		assertArrayEquals(type.getExtensions(), new String[]{"foo"});
+    // By Type
+    MimeType type = mimeTypes.getByType("application/vnd.custom.foo");
+    assertEquals(type.getMimeType(), "application/vnd.custom.foo");
+    assertArrayEquals(type.getExtensions(), new String[]{"foo"});
 
-		// By ext
-		type = mimeTypes.getByExtension("foo");
-		assertEquals(type.getMimeType(), "application/vnd.custom.foo");
-		assertArrayEquals(type.getExtensions(), new String[]{"foo"});
-	}
+    // By ext
+    type = mimeTypes.getByExtension("foo");
+    assertEquals(type.getMimeType(), "application/vnd.custom.foo");
+    assertArrayEquals(type.getExtensions(), new String[]{"foo"});
+  }
 
-	@Test
-	public void testCustomDefinitionsInline() {
-		MimeTypes mimeTypes = MimeTypes.getInstance();
-		mimeTypes.loadOne("application/vnd.custom.bar	 bar zot");
+  @Test
+  public void testCustomDefinitionsInline() {
+    MimeTypes mimeTypes = MimeTypes.getInstance();
+    mimeTypes.loadOne("application/vnd.custom.bar	 bar zot");
 
-		// By Type
-		MimeType type = mimeTypes.getByType("application/vnd.custom.bar");
-		assertEquals(type.getMimeType(), "application/vnd.custom.bar");
-		assertArrayEquals(type.getExtensions(), new String[]{"bar", "zot"});
+    // By Type
+    MimeType type = mimeTypes.getByType("application/vnd.custom.bar");
+    assertEquals(type.getMimeType(), "application/vnd.custom.bar");
+    assertArrayEquals(type.getExtensions(), new String[]{"bar", "zot"});
 
-		// By ext
-		type = mimeTypes.getByExtension("bar");
-		assertEquals(type.getMimeType(), "application/vnd.custom.bar");
-		assertArrayEquals(type.getExtensions(), new String[]{"bar", "zot"});
-	}
+    // By ext
+    type = mimeTypes.getByExtension("bar");
+    assertEquals(type.getMimeType(), "application/vnd.custom.bar");
+    assertArrayEquals(type.getExtensions(), new String[]{"bar", "zot"});
+  }
 
-	@Test
-	public void testCustomDefinitionsRegister() {
-		MimeTypes mimeTypes = MimeTypes.getInstance();
-		mimeTypes.register(new MimeType("application/vnd.custom.orbit", new String[]{"orb"}));
+  @Test
+  public void testCustomDefinitionsRegister() {
+    MimeTypes mimeTypes = MimeTypes.getInstance();
+    mimeTypes.register(new MimeType("application/vnd.custom.orbit", new String[]{"orb"}));
 
-		// By Type
-		MimeType type = mimeTypes.getByType("application/vnd.custom.orbit");
-		assertEquals(type.getMimeType(), "application/vnd.custom.orbit");
-		assertArrayEquals(type.getExtensions(), new String[]{"orb"});
+    // By Type
+    MimeType type = mimeTypes.getByType("application/vnd.custom.orbit");
+    assertEquals(type.getMimeType(), "application/vnd.custom.orbit");
+    assertArrayEquals(type.getExtensions(), new String[]{"orb"});
 
-		// By ext
-		type = mimeTypes.getByExtension("orb");
-		assertEquals(type.getMimeType(), "application/vnd.custom.orbit");
-		assertArrayEquals(type.getExtensions(), new String[]{"orb"});
-	}
+    // By ext
+    type = mimeTypes.getByExtension("orb");
+    assertEquals(type.getMimeType(), "application/vnd.custom.orbit");
+    assertArrayEquals(type.getExtensions(), new String[]{"orb"});
+  }
 
-	@Test
-	public void testSameInstance() {
-		MimeTypes mimeTypes = MimeTypes.getInstance();
+  @Test
+  public void testSameInstance() {
+    MimeTypes mimeTypes = MimeTypes.getInstance();
 
-		MimeType type1 = mimeTypes.getByType("text/css");
-		MimeType type2 = mimeTypes.getByExtension("css");
-		assertTrue(type1 == type2);
-	}
+    MimeType type1 = mimeTypes.getByType("text/css");
+    MimeType type2 = mimeTypes.getByExtension("css");
+    assertTrue(type1 == type2);
+  }
 
-	@Test
-	public void testIdentity() {
-		MimeType json1 = new MimeType("application/json", new String[]{"json"});
-		MimeType json2 = new MimeType("application/json", new String[]{"json"});
-		assertEquals(json1, json2);
-		assertEquals(json1.hashCode(), json2.hashCode());
+  @Test
+  public void testIdentity() {
+    MimeType json1 = new MimeType("application/json", new String[]{"json"});
+    MimeType json2 = new MimeType("application/json", new String[]{"json"});
+    assertEquals(json1, json2);
+    assertEquals(json1.hashCode(), json2.hashCode());
 
-		// Same ext, different mime type
-		MimeType githubJson = new MimeType("application/vnd.github+json", new String[]{"json"});
-		assertNotEquals(githubJson, json1);
-		assertNotEquals(githubJson.hashCode(), json1.hashCode());
+    // Same ext, different mime type
+    MimeType githubJson = new MimeType("application/vnd.github+json", new String[]{"json"});
+    assertNotEquals(githubJson, json1);
+    assertNotEquals(githubJson.hashCode(), json1.hashCode());
 
-		// Same mime type, different ext
-		MimeType jsonMl = new MimeType("application/json", new String[]{"jsonml"});
-		assertNotEquals(jsonMl, json1);
-		assertNotEquals(jsonMl.hashCode(), json1.hashCode());
-	}
+    // Same mime type, different ext
+    MimeType jsonMl = new MimeType("application/json", new String[]{"jsonml"});
+    assertNotEquals(jsonMl, json1);
+    assertNotEquals(jsonMl.hashCode(), json1.hashCode());
+  }
 
-	@Test
-	public void testNonExisting() {
-		assertNull(MimeTypes.getInstance().getByType("application/vnd.imaginary"));
-		assertNull(MimeTypes.getInstance().getByExtension("none"));
-	}
+  @Test
+  public void testNonExisting() {
+    assertNull(MimeTypes.getInstance().getByType("application/vnd.imaginary"));
+    assertNull(MimeTypes.getInstance().getByExtension("none"));
+  }
 }
